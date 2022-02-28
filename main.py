259ab329bca80f65cb1f8c7400182fcab56fd827
AngryPotat0 <211957142@qq.com>
2022-02-28 15:26:08 +0800

    render class

--- main.py
+++ main.py
@@ -2,25 +2,34 @@ from Lexer import *
 from Parser import *
 from Compiler import *
 
-text = '''
+class Render:
+    def __init__(self,template) -> None:
+        self.template = template
+        self.render_functon = ""
+
+    def compile(self):
+        lex = Lexer(self.template)
+        p = Parser(lex.lexer())
+        ast = p.parser()
+        compiler = Compiler()
+        self.render_functon = compiler.compile(ast)
+
+    def render(self,context):
+        if(self.render_functon == ''):
+            return None
+        functions = {}
+        exec(self.render_functon,functions)
+        return functions["render_function"](context)
+
+
+template = '''
 <p>{{ viewsDict }}</p>
-<p>{{ viewsDict.name }}</p>
+<p>{{ viewsDict.name | abs}}</p>
 '''
-
-lex = Lexer(text)
-lis = lex.lexer()
-for token in lis:
-    print(token)
-
-p = Parser(lis)
-template = p.parser()
-print(template)
-
-compiler = Compiler()
-code = compiler.compile(template)
-print(code)
-
-views_dict = {"name":"AAC"}
+views_dict = {"name":-123}
 context = {"viewsDict":views_dict}
-func = exec(code)
-print(render_function(context))
+
+render = Render(template)
+render.compile()
+html = render.render(context)
+print(html)
--- test.py
+++ test.py
@@ -1,12 +1,24 @@
-python_source = """\
-SEVENTEEN = 17
+# python_source = """\
+# SEVENTEEN = 17
 
-def three():
-    return 3
-"""
+# def three():
+#     return 3
+# """
 # global_namespace = {}
 # exec(python_source, global_namespace)
 # print(global_namespace['SEVENTEEN'])
 # print(global_namespace['three']())
-t = "12"
-print("""result.append(%s)""" % repr(t))
+# t = "12"
+# print("""result.append(%s)""" % repr(t))
+
+code = """
+def render_function(context):
+    c_viewsDict = context['viewsDict']
+    result = []
+    result.extend(['<p>',str(c_viewsDict),'</p>\n<p>',str(abs(c_viewsDict['name'])),'</p>\n'])
+    return "".join(result)
+"""
+
+functions = {}
+exec(code,functions)
+print(functions)
